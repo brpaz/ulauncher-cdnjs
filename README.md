87f09417908adfe6b4701ea1e6052c8d2f2b7efa
Bruno Paz <brunopaz@sapo.pt>
2019-06-22 11:52:07 +0100

    Migrate to Python 3 and new Ulauncher API

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Ulauncher cdnjs
 
+[![Ulauncher Extension](https://img.shields.io/badge/Ulauncher-Extension-green.svg?style=for-the-badge)](https://ext.ulauncher.io/-/github-brpaz-ulauncher-cdnjs)
+[![CircleCI](https://img.shields.io/circleci/build/github/brpaz/ulauncher-cdnjs.svg?style=for-the-badge)](https://circleci.com/gh/brpaz/ulauncher-cdnjs)
+![License](https://img.shields.io/github/license/brpaz/ulauncher-cdnjs.svg?style=for-the-badge)
+
 > [ulauncher](https://ulauncher.io/) Extension to quickly search frontend libraries on [cndjs](https://cdnjs.com).
 
 ## Usage
@@ -8,8 +12,8 @@
 
 ## Requirements
 
-* [ulauncher](https://ulauncher.io/)
-* Python >= 2.7
+* [ulauncher 5](https://ulauncher.io/)
+* Python >= 3
 
 ## Install
 
@@ -33,6 +37,17 @@ ln -s <repo_location> ulauncher-cdnjs
 
 To see your changes, stop ulauncher and run it from the command line with: ```ulauncher -v```.
 
+## Contributing
+
+Contributions, issues and Features requests are welcome.
+
+## Show your support
+
+<a href="https://www.buymeacoffee.com/Z1Bu6asGV" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
+
+
 ## License 
 
-MIT
+Copywright @ 2019 [Bruno Paz](https://github.com/brpaz)
+
+This project is [MIT](LLICENSE) Licensed.
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+Ulauncher CDNjs Extension
+Search packages in cdnjs website directly from Ulauncher
+"""
+
 import logging
 import requests
 from ulauncher.api.client.Extension import Extension
@@ -9,57 +14,80 @@ from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
 from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAction
 
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 CDN_URL = "https://api.cdnjs.com/libraries/"
 
+
 class CdnjsExtension(Extension):
+    """ Main Class """
 
     def __init__(self):
-        logger.info('init cdnjs Extension')
+        LOGGER.info('init cdnjs Extension')
         super(CdnjsExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
 
+
 class KeywordQueryEventListener(EventListener):
+    """
+    Query Event listener
+    """
 
     def on_event(self, event, extension):
+        """ Handles Keyword Event """
         items = []
-      
+
         try:
 
-            maxResults = int(extension.preferences['max_results'])
-            payload = {'search': event.get_argument(
-            ), 'fields': 'version,description,repository'.encode('utf-8')}
+            query = event.get_argument() or ""
+
+            if len(query) < 3:
+                return RenderResultListAction([
+                    ExtensionResultItem(
+                        icon='images/icon.png',
+                        name="Keep Typing to search on cdn.js ...",
+                        on_enter=HideWindowAction(),
+                        highlightable=False,
+                    )
+                ])
+
+            max_results = int(extension.preferences['max_results'])
+            payload = {'search': query,
+                       'fields': 'version,description,repository'.encode('utf-8')}
 
             headers = {'Content-Type': 'application/json',
-                    'User-Agent': 'Ulauncher-cdnjs/1.0'}
+                       'User-Agent': 'Ulauncher-cdnjs/2.0'}
 
-            r = requests.get(CDN_URL, params=payload, headers=headers)
-            r.raise_for_status()
+            req = requests.get(CDN_URL, params=payload, headers=headers)
+            req.raise_for_status()
 
-            libraries = r.json()
+            libraries = req.json()
 
-            for library in libraries['results'][0:maxResults]:
-                repoUrl = library['repository']['url'].replace(
+            for library in libraries['results'][0:max_results]:
+                repo_url = library['repository']['url'].replace(
                     'git://', 'https://').replace('git+', '')
                 items.append(ExtensionResultItem(
                     icon='images/icon.png',
                     name=library['name'] + " (" + library['version'] + ")",
                     description=library['description'],
                     on_enter=CopyToClipboardAction(library['latest']),
-                    on_alt_enter=OpenUrlAction(repoUrl)
-            ))
+                    on_alt_enter=OpenUrlAction(repo_url)
+                ))
 
-        except requests.exceptions.RequestException as err:
-            logger.error(err.message)
-            items.append(ExtensionResultItem(
-                icon='images/icon.png',
-                name="Request error: " + r.status_code,
-                description=err.message,
-                on_enter=HideWindowAction
-            ))
-        finally:
             return RenderResultListAction(items)
 
+        except requests.exceptions.HTTPError as err:
+            LOGGER.error(err)
+            items.append(ExtensionResultItem(
+                icon='images/icon.png',
+                name="Error requesting CDNjs",
+                description="Request error: " + str(req.status_code),
+                on_enter=HideWindowAction(),
+                highlightable=False,
+            ))
+
+            return RenderResultListAction(items)
+
+
 if __name__ == '__main__':
-   CdnjsExtension().run()
+    CdnjsExtension().run()
